b7027de5a7ede7cf0fea4857b52e5d5f1d88e1a0
NavneetNandan <navneet.nandan8@gmail.com>
2017-04-09 10:45:21 +0530

    generalized for all type of sentences

--- em_for_IBM_1.py
+++ em_for_IBM_1.py
@@ -3,29 +3,33 @@ import pandas
 import nltk
 
 sentence_pairs = np.array([["das haus", "the house"], ["das buch", "the book"], ["ein buch", "a book"]])
-translation_prob = {'das': {'house': 0.25, 'the': 0.25, 'book': 0.25, 'a': 0.25},
-                    'haus': {'house': 0.25, 'the': 0.25, 'book': 0.25, 'a': 0.25},
-                    'buch': {'house': 0.25, 'the': 0.25, 'book': 0.25, 'a': 0.25},
-                    'ein': {'house': 0.25, 'the': 0.25, 'book': 0.25, 'a': 0.25}}
-count_conditional = {'das': {'house': 0, 'the': 0, 'book': 0, 'a': 0},
-                     'haus': {'house': 0, 'the': 0, 'book': 0, 'a': 0},
-                     'buch': {'house': 0, 'the': 0, 'book': 0, 'a': 0},
-                     'ein': {'house': 0, 'the': 0, 'book': 0, 'a': 0}}
+translation_prob = {}
+count_conditional = {}
+all_english = set()
+all_german = set()
+for eng_sentence in sentence_pairs[:, 1]:
+    for eng_word in nltk.tokenize.casual_tokenize(eng_sentence):
+        all_english.add(eng_word)
+for german_sentence in sentence_pairs[:, 0]:
+    for german_word in nltk.tokenize.casual_tokenize(german_sentence):
+        all_german.add(german_word)
+uniform_translation_prob = 1 / len(all_english)
+for german_word in all_german:
+    translation_prob[german_word] = {}
+    count_conditional[german_word] = {}
+    for eng_word in all_english:
+        translation_prob[german_word][eng_word] = uniform_translation_prob
+        count_conditional[german_word][eng_word] = 0.0
 translation_prob = pandas.DataFrame.from_dict(translation_prob)
-all_german = translation_prob.axes[1]
-all_english = translation_prob.axes[0]
 count_conditional = pandas.DataFrame.from_dict(count_conditional)
-print(count_conditional)
-count = 0
 total = pandas.Series(index=all_german, data=0.0)
 s_total = pandas.Series(index=all_english, data=0.0)
-
 
 def zero(a):
     return 0.0
 
-
-while count <= 30:
+count = 0
+while count <= 5:
     count_conditional = count_conditional.applymap(func=zero)
     total = total.apply(func=zero)
     for sentence_pair in sentence_pairs:
@@ -38,7 +42,7 @@ while count <= 30:
                 count_conditional.loc[eng_word, german_word] += translation_prob[german_word][eng_word] / s_total[
                     eng_word]
                 total[german_word] += translation_prob[german_word][eng_word] / s_total[eng_word]
-    print("coiukjb")
+    print("count_conditional")
     print(count_conditional)
     print("total")
     print(total)
